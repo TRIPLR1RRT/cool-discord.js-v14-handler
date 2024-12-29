b63dc11aa1e5947dd29a007767e6191eb8da48a9
Sahil Khan <83149395+TRIPLR1RRT@users.noreply.github.com>
2024-12-29 20:07:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    client_token: "", //your bot's token here
+    client_id: "", //client's id here, keep proper bot's id or it'll show you error
+    client_prefix: "+",
+    mongodb_url: "", //optional
+    developers: ["783972385882767400", "1050641070368772166"], // add you'r id in here
+    sharding: false, //as you want :)
+    database: false, //as you want :)
+}
+
+/**
+ * Get discord bot token from here https://discord.com/developers/applications
+ * Get mongodb url from https://www.mongodb.com/
+ */
